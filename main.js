9d4671604c594107b116d2f4ec5aaf068d1d8cc9
Yu Sang <yu.sang@cgi.com>
2024-08-23 16:32:02 -0300

    update example

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
+const { WebContentsView, BaseWindow } = require("electron");
 const { app, BrowserWindow, ipcMain, nativeTheme } = require("electron/main");
 // include the Node.js 'path' module at the top of your file
 const path = require("node:path");
 
 const createWindow = () => {
-  const win = new BrowserWindow({
+  const win = new BaseWindow({
     width: 1200,
     height: 900,
+
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
     backgroundColor: "#fff",
+  });
+
+  const view1 = new WebContentsView();
+  win.contentView.addChildView(view1);
+  view1.webContents.loadURL("https://google.com");
+  view1.setBounds({
+    x: 0,
+    y: 0,
+    width: 1200,
+    height: 800,
   });
 
   win.loadFile("index.html");
